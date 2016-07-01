50fd437a10ea39da7e58366dfaf38da88da56757
Ben Flores <benjaminhyw@gmail.com>
2016-06-30 20:52:53 -0700

    figured out how to replace content of divs

--- public/js/application.js
+++ public/js/application.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
   $("#location-form").on("submit", function(event){
     event.preventDefault();
     $(this).hide('slow')
+
+    $("#forecast-container").empty()
+    $("#forecast-container").html("howdy")
   })
 
 
