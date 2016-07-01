687cd10b21d14040d363d305b0638ed8b9958ffb
Ben Flores <benjaminhyw@gmail.com>
2016-06-30 21:20:24 -0700

    my logic for changing backgrounds works! now just have to complete if else statement

--- public/js/application.js
+++ public/js/application.js
@@ -2,16 +2,23 @@ $(document).ready(function(){
 
   $("#location-form").on("submit", function(event){
     event.preventDefault();
-    $(this).hide('slow')
+    // $(this).hide('slow')
 
     var temp = $("#forecast-container").html()
     console.log(temp)
 
-    if ( _.range(90,150).includes(parseInt(temp))) {
+    if ( _.range(100,150).includes(parseInt(temp))) {
       $("body").removeClass()
       $("body").addClass("volcano")
 
-      console.log("kinda but not really working")
+      console.log("volcano")
+    } else if (_.range(90,100).includes(parseInt(temp))){
+      
+      $("body").removeClass()
+      $("body").addClass("on_fire")
+
+      console.log("on_fire")
+
     }
 
     // $("#forecast-container").empty()
